ef3e914ca5b5e3781a8e5681d344f991a10146ac
Brian Matthews <brian@btmatthews.com>
2013-08-31 15:43:13 +0100

    Enabling unit testing and logging

--- janus-server/src/main/java/com/btmatthews/janus/server/IndexController.java
+++ janus-server/src/main/java/com/btmatthews/janus/server/IndexController.java
@@ -1,13 +1,18 @@
 package com.btmatthews.janus.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping("index.html")
     public String index() {
+        LOGGER.info("Rendering /index.html");
         return "index";
     }
 }
